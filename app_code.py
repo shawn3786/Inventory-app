004f3d2006f1ee1d610908f6144d2aaa2e68a4f2
Muhammad Zeeshan <50868547+shawn3786@users.noreply.github.com>
2025-07-15 15:46:37 +0200

    Update app_code.py

--- app_code.py
+++ app_code.py
@@ -215,11 +215,11 @@ elif st.session_state.page == " Add Finished Stock":
     st.title("Please Write the Name of items Which you are thinking its going to be finished")
     qty = st.text_input("Write the name of items:", key="finished_item_input")
     FINISHED_FILE = "Finished Items.txt"
-   if st.button("💾 Save"):
-    if qty.strip() != "":
-        with open(FINISHED_FILE, "a") as f:
-            f.write(qty.strip() + "\n")
-            st.write("Items saved successfully")
+    if st.button("💾 Save"):
+        if qty.strip() != "":
+            with open(FINISHED_FILE, "a") as f:
+                f.write(qty.strip() + "\n")
+                st.write("Items saved successfully")
     if st.button("Main Menu "):
             st.session_state.page = "menu"
 
