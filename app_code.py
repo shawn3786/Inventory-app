b52c3e89b497c6e1d1d0ce87e0ed54477fdf5eac
Muhammad Zeeshan <50868547+shawn3786@users.noreply.github.com>
2025-07-15 15:38:18 +0200

    Update app_code.py

--- app_code.py
+++ app_code.py
@@ -214,6 +214,7 @@ elif st.session_state.page == "inventory": # Changed to lowercase 'inventory' fo
 elif st.session_state.page == " Add Finished Stock":
     st.title("Please Write the Name of items Which you are thinking its going to be finished")
     qty = st.text_input("Write the name of items:", key="finished_item_input")
+    FINISHED_FILE = "Finished Items.txt"
     with open(FINISHED_FILE, "a") as f:
         f.write(qty)
 
